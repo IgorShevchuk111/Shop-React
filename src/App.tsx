c24b8cea3bf157fe3d3a45d04827389c75b71cd9
Igor Shevchuk <igorshevchuk111@gmail.com>
2024-05-09 20:58:40 +0100

    App main Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,32 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import MainLayout from './layouts/MainLayout';
 import AuthLayout from './layouts/AuthLayout';
-import HomePage from './pages/HomePage';
+import HomePage from './pages/HomePage/HomePage';
 import ProductPage from './pages/ProductPage';
 import LoginPage from './pages/LoginPage';
 import BasketPage from './pages/BasketPage';
 import ErrorPage from './pages/ErrorPage';
 import BasketLayout from './layouts/BasketLayout';
 
-
 function App() {
-
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<HomePage />} />
-          <Route path="/:category" element={<ProductPage />} />
-        </Route>
-        <Route path="/login" element={<AuthLayout />}>
-          <Route index element={<LoginPage />} />
-        </Route> 
-        <Route path="/basket" element={<BasketLayout />}>
-          <Route index element={<BasketPage />} />
-        </Route>
-          <Route path="/*" element={<ErrorPage />} />
-      </Routes>
-  </BrowserRouter>
-  )
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<MainLayout />}>
+					<Route index element={<HomePage />} />
+					<Route path="/:category" element={<ProductPage />} />
+				</Route>
+				<Route path="/login" element={<AuthLayout />}>
+					<Route index element={<LoginPage />} />
+				</Route>
+				<Route path="/basket" element={<BasketLayout />}>
+					<Route index element={<BasketPage />} />
+				</Route>
+				<Route path="/*" element={<ErrorPage />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
-export default App
+export default App;
