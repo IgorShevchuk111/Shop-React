ae849836bdddaca08aab8185ea595e023d33928c
Igor Shevchuk <igorshevchuk111@gmail.com>
2024-05-22 15:10:56 +0100

    Create UsedSmartphonesSection

---
+++ src/components/Main/UsedProductsSection/UsedProductsSection.tsx
@@ -0,0 +1,21 @@
+import { Row, Col } from 'react-bootstrap';
+import SortByDropdown from '../SortByDropdown/SortByDropdown';
+
+const UsedSmartphonesSection = () => {
+	return (
+		<Row className="m-5">
+			<Col className="text-sm-center text-xl-start" xl={9}>
+				<h3 className="font-weight-bold">Used Smartphones</h3>
+				<p>Buy Used phones for cheap when you shop at Online-Shop.</p>
+			</Col>
+			<Col
+				lg={3}
+				className="d-none d-xl-flex align-items-center justify-content-end"
+			>
+				<SortByDropdown />
+			</Col>
+		</Row>
+	);
+};
+
+export default UsedSmartphonesSection;
