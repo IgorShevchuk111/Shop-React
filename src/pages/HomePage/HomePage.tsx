46c9f4b93f35d0a088d581585fbde12f5c52680f
Igor Shevchuk <igorshevchuk111@gmail.com>
2024-05-22 17:04:39 +0100

    Create HomePage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,28 +1,27 @@
 import { useEffect, useState } from 'react';
 
-import classes from './HomePage.module.scss';
+import './HomePage.scss';
 import '../../index.scss';
 import { data } from '../../services/data';
-import GalleryCarousel from '../../components/GalleryCarousel/GalleryCarousel';
+import GalleryCarousel from '../../components/Shared/GalleryCarousel/GalleryCarousel';
 import { imageUrls } from '../../services/data';
 
 function HomePage() {
 	const [currentImageIndex, setCurrentImageIndex] = useState(0);
-	// Interval Banner
+	//  ------- Interval Banner ---------
 	useEffect(() => {
 		const intervalId = setInterval(() => {
 			setCurrentImageIndex(prevIndex => (prevIndex + 1) % imageUrls.length);
 		}, 3000);
 		return () => clearInterval(intervalId);
 	}, []);
-
 	return (
 		<div>
-			{/* ad */}
-			<section className={`${classes.ad}`}>
+			{/* --------- ad --------- */}
+			<section className="ad">
 				Trade tech you don't want for cash you do.
 			</section>
-			{/* Banner */}
+			{/* --------- Banner --------*/}
 			<section>
 				<img
 					width="100%"
@@ -30,12 +29,12 @@ function HomePage() {
 					src={imageUrls[currentImageIndex]}
 				></img>
 			</section>
-			{/* Gallery smartphones */}
+			{/* -------- Gallery smartphones --------*/}
 			<section>
 				<h3 className="my-4 text-center">Most wanted smartphones</h3>
 				<GalleryCarousel models={Object.values(data.smartPhones)} />
 			</section>
-			{/* Gallery laptops */}
+			{/* -------- Gallery laptops ---------*/}
 			<section>
 				<h3 className="my-4 text-center">Most popular laptops</h3>
 				<GalleryCarousel models={Object.values(data.laptops)} />
