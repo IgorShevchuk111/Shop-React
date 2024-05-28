3ebe78c8d5fc496ebe803d48a1db0eb99877c96a
Igor Shevchuk <igorshevchuk111@gmail.com>
2024-05-28 20:46:37 +0100

    Create ItemDetails

---
+++ src/pages/ItemPage/components/ItemDetails.tsx
@@ -0,0 +1,24 @@
+import { Col } from 'react-bootstrap';
+import ReactBootstrapAccordion from './ReactBootstrapAccordion/ReactBootstrapAccordion';
+import ItemHeader from './ItemHeader/ItemHeader';
+import ItemActions from './ItemActions';
+import ItemInfo from './ItemInfo';
+
+interface ItemDetailsProps {
+	model: string;
+	price: number;
+}
+
+function ItemDetails({ model, price }: ItemDetailsProps) {
+	return (
+		<Col lg={6}>
+			<ItemHeader model={model} price={price} />
+			<ItemActions />
+			<hr />
+			<ItemInfo />
+			<ReactBootstrapAccordion />
+		</Col>
+	);
+}
+
+export default ItemDetails;
