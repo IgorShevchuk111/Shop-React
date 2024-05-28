1f541ed6203d605308ce00d099fda92dee56b45a
Igor Shevchuk <igorshevchuk111@gmail.com>
2024-05-28 20:41:18 +0100

    Create ItemPage

---
+++ src/pages/ItemPage/ItemPage.tsx
@@ -0,0 +1,37 @@
+import Breadcrumb from '../../components/Shared/Breadcrumb/Breadcrumb';
+import { useEffect, useState } from 'react';
+import { getProducts } from '../../services/data';
+import { AnyProduct, Data } from '../../types';
+import './ItemPage.scss';
+import { Container, Row } from 'react-bootstrap';
+import ItemImg from './components/ItemImg/ItemImg';
+import ItemDetails from './components/ItemDetails/ItemDetails';
+
+function ItemPage() {
+	const [item, setItem] = useState<AnyProduct | null>(null);
+	const pathArray = location.pathname.split('/');
+	const category = pathArray[1];
+	const itemModel = pathArray[pathArray.length - 1].replace(/-/g, ' ');
+
+	useEffect(() => {
+		const item = getProducts(category as keyof Data).find(
+			item => item.model === itemModel
+		);
+		setItem(item);
+	}, [category, itemModel]);
+	return (
+		<div className="itemPage-container">
+			<Breadcrumb />
+			<Container>
+				{item && (
+					<Row>
+						<ItemImg img={item.img} model={item.model} />
+						<ItemDetails model={item.model} price={item.price} />
+					</Row>
+				)}
+			</Container>
+		</div>
+	);
+}
+
+export default ItemPage;
