2dd3ceb59dd7530fb72245c6781dc8d46ccbd093
Igor Shevchuk <igorshevchuk111@gmail.com>
2024-05-29 15:39:52 +0100

    Refactor Products

--- src/components/Main/Products/Products.tsx
+++ src/components/Main/Products/Products.tsx
@@ -1,22 +1,14 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import ProductCard from '../../Shared/ProductCard/ProductCard';
 import { ProductsProps } from '../../../types';
+import ProductCard from '../../Shared/ProductCard/ProductCard';
 
 function Products({ filteredProducts }: ProductsProps) {
 	return (
 		<Container>
 			<Row className="g-3">
 				{filteredProducts.map(item => (
-					<Col
-						key={item.id}
-						xs={{ span: 8, offset: 2 }}
-						sm={{ span: 6, offset: 0 }}
-						lg={4}
-					>
-						<ProductCard item={item} />
-					</Col>
+					<ProductCard key={item.id} item={item} />
 				))}
 			</Row>
 		</Container>
